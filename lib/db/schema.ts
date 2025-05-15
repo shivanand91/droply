30469bb9d9e18723a80aa0e56c4fd64d8696bbb4
Shivanand Kumar <132545822+shivanand91@users.noreply.github.com>
2025-05-15 17:55:46 +0530

    this is the setup of drizzele orm

--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -1,4 +1,4 @@
-import { integer, pgTable, text, uuid, boolean} from 'drizzle-orm/pg-core'
+import { integer, pgTable, text, uuid, boolean, timestamp} from 'drizzle-orm/pg-core'
 import { relations } from 'drizzle-orm'
 
 export const files = pgTable("files", {
@@ -17,8 +17,26 @@ export const files = pgTable("files", {
     // ownership info
 
     ownerId: uuid("user_id").notNull(), // user ID of the file owner
-    parentId: uuid("parent_id").notNull(), // parent folder ID (if any)
+    parentId: uuid("parent_id"), // parent folder ID (if any)
 
     // file / folder flags
     isFolder: boolean("is_folder").notNull().default(false), // is this a folder?
+    isStarred: boolean("is_starred").notNull().default(false), // is this file starred?
+    isTrash: boolean("is_t rash").notNull().default(false), // is this file in trash?
+
+    //Timestamps 
+    createdAt: timestamp("created_at").defaultNow().notNull(), // creation timestamp
+    updatedAt: timestamp("updated_at").defaultNow().notNull(), // last update timestamp
 })
+
+export const filesRelations = relations(files, ({ one, many }) => ({
+    parent: one(files, {
+        fields: [files.parentId],
+        references: [files.id],
+    }),
+    children: many(files), 
+}))
+
+// type definition for the files table
+export const File = typeof files.$inferSelect 
+export const NewFile = typeof files.$inferInsert
